5ae15311b630d938fe4b60cef1acca9cba0e0cb6
MAA91 <Sanya13082007@gmail.com>
2024-05-23 00:02:40 +0300

    All Done

--- IntrodactionToOOP/main.cpp
+++ IntrodactionToOOP/main.cpp
@@ -1,10 +1,58 @@
 //IntrodactionToOOP
 #include"stdafx.h"
 
-using namespace std;
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	} 
+	double distance(double x, double y)const
+	{
+		return sqrt(pow(this->x - x, 2) + pow(this->y - y, 2));
+	}
+};
+
+double distance(Point A, Point B)
+{
+	//return sqrt(pow(A.get_x() - B.get_x(), 2) + pow(A.get_y() - B.get_y(), 2));
+	return A.distance(B.get_x(), B.get_y());
+}
+
+//#define STRUCT_POINT
 
 void main()
 {
 	setlocale(0, "");
-	cout << "Hello OOP";
+#ifdef STRUCT_POINT
+	Point A; // Объявление переменной 'A' типа 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+	Point A, B;
+	A.set_x(2);
+	A.set_y(3);
+	B.set_x(6);
+	B.set_y(6);
+	cout << A.distance(6, 6) << endl;
+	cout << distance(A, B) << endl;
 }
